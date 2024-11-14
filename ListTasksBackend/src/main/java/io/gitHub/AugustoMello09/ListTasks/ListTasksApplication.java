545f289130cbfe946332e8eb4ecfcec337bd5c68
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-11-14 12:21:31 -0300

    project created

---
+++ ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/ListTasksApplication.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.ListTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListTasksApplication.class, args);
+	}
+
+}

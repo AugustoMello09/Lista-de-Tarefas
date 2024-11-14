75268ef86a1e6a450908d012a44f077f9853a9e0
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-11-14 14:56:09 -0300

    feat : Tarefa Repository

---
+++ ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/domain/repositories/TarefaRepository.java
@@ -0,0 +1,11 @@
+package io.gitHub.AugustoMello09.ListTasks.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.gitHub.AugustoMello09.ListTasks.domain.entities.Tarefa;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long>{
+
+}

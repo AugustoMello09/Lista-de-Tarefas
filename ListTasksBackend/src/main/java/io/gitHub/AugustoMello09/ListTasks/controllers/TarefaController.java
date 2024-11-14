8f0e0d686b0b90b0205246f3e2ab8ebaa1a18ba9
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-11-14 17:18:38 -0300

    feat: add create method in TarefaController

--- ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/controllers/TarefaController.java
+++ ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/controllers/TarefaController.java
@@ -1,14 +1,19 @@
 package io.gitHub.AugustoMello09.ListTasks.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.gitHub.AugustoMello09.ListTasks.domain.entities.dtos.TarefaDTO;
+import io.gitHub.AugustoMello09.ListTasks.domain.entities.dtos.TarefaRecord;
 import io.gitHub.AugustoMello09.ListTasks.servicies.TarefaService;
 import lombok.RequiredArgsConstructor;
 
@@ -29,6 +34,16 @@ public class TarefaController {
 	public ResponseEntity<List<TarefaDTO>> listAll(){
 		var response = service.listAll();
 		return ResponseEntity.ok().body(response);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TarefaDTO> create(@RequestBody TarefaRecord tarefa){
+		var newObj = service.create(tarefa);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(newObj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(newObj);
 	}
 
 }

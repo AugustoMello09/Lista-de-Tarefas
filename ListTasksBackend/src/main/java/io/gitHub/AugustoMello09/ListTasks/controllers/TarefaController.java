c32950ded8564e40e7d51529a762a814964b6200
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-11-14 20:26:47 -0300

    feat: add update method in TarefaController

--- ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/controllers/TarefaController.java
+++ ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/controllers/TarefaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,12 @@ public class TarefaController {
 				.buildAndExpand(newObj.getId())
 				.toUri();
 		return ResponseEntity.created(uri).body(newObj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody TarefaRecord tarefa,@PathVariable Long id){
+		service.update(tarefa, id);
+		return ResponseEntity.ok().build();
 	}
 
 }

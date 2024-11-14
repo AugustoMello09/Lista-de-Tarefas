48bb7929a1e8836732144878312bbaa41d22aeb5
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-11-14 16:22:48 -0300

    test: add integration test for findById endpoint in TarefaController

---
+++ ListTasksBackend/src/test/java/io/gitHub/AugustoMello09/ListTasks/controllers/TarefaControllerTest.java
@@ -0,0 +1,57 @@
+package io.gitHub.AugustoMello09.ListTasks.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import io.gitHub.AugustoMello09.ListTasks.domain.entities.dtos.TarefaDTO;
+import io.gitHub.AugustoMello09.ListTasks.provider.TarefaDTOProvider;
+import io.gitHub.AugustoMello09.ListTasks.servicies.TarefaService;
+
+@ExtendWith(MockitoExtension.class)
+public class TarefaControllerTest {
+
+	private static final long ID = 1L;
+
+	@InjectMocks
+	private TarefaController controller;
+
+	@Mock
+	private TarefaService service;
+
+	// private TarefaProvider tarefaProvider;
+	private TarefaDTOProvider tarefaDTOProvider;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+		// tarefaProvider = new TarefaProvider();
+		tarefaDTOProvider = new TarefaDTOProvider();
+		controller = new TarefaController(service);
+	}
+
+	@DisplayName("Deve retornar uma tarefa. ")
+	@Test
+	public void shouldControllerReturnFindByIdWithSuccess() {
+		TarefaDTO tarefaDTO = tarefaDTOProvider.criar();
+		when(service.findById(ID)).thenReturn(tarefaDTO);
+		var response = controller.findById(ID);
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(TarefaDTO.class, response.getBody().getClass());
+	}
+
+}

2c4681f97d07429013b4c600058b76e2312f5da2
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-11-22 23:29:33 -0300

    feat: add max length validation to task name

--- ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/domain/entities/dtos/TarefaRecord.java
+++ ListTasksBackend/src/main/java/io/gitHub/AugustoMello09/ListTasks/domain/entities/dtos/TarefaRecord.java
@@ -4,7 +4,8 @@ import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record TarefaRecord(@NotBlank(message = "Campo obrigatório") String name, @NotNull(message = "Campo obrigatório") BigDecimal cost, @NotBlank(message = "Campo obrigatório") String dueDate) {
+public record TarefaRecord(@NotBlank(message = "Campo obrigatório") @Size(max = 91, message = "máximo de caracteres é 91") String name, @NotNull(message = "Campo obrigatório") BigDecimal cost, @NotBlank(message = "Campo obrigatório") String dueDate) {
 
 }

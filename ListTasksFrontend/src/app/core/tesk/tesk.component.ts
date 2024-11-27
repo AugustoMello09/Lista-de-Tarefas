be71e31e2c10eeff594dea477e27175c9eb710e8
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-11-27 15:50:31 -0300

    feat : ts task work

--- ListTasksFrontend/src/app/core/tesk/tesk.component.ts
+++ ListTasksFrontend/src/app/core/tesk/tesk.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Tarefa } from '../shared/model/tarefa.model';
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { CriarTarefaComponent } from '../shared/components/criar-tarefa/criar-tarefa.component';
+import { TarefaService } from '../service/tarefa.service';
+import { ExcluirTarefaComponent } from '../shared/components/excluir-tarefa/excluir-tarefa.component';
+import { UpdateTarefaComponent } from '../shared/components/update-tarefa/update-tarefa.component';
 
 @Component({
   selector: 'app-tesk',
@@ -16,13 +19,21 @@ export class TeskComponent implements OnInit {
   public dragging: boolean = false;
   public draggedIndex: number | null = null;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog,
+    private service: TarefaService
+  ) { }
 
   ngOnInit(): void {
+    this.service.listAll();
+    this.service.tarefas$.subscribe((tarefas) => { 
+      this.tarefas = tarefas;
+    })
   }
 
   public deleteTarefa(id: number): void {
-    
+    this.dialog.open(ExcluirTarefaComponent, {
+      data: { id: id }
+    });
   }
 
   public open(): void {
@@ -30,22 +41,48 @@ export class TeskComponent implements OnInit {
   }
 
   public atualizarTarefa(id: number): void {
-   
+    this.dialog.open(UpdateTarefaComponent, { data: { id: id } })
+      .afterClosed().subscribe((updatedTask: Tarefa | null) => {
+        if (updatedTask) {
+          this.service.patchUpdate(id, updatedTask); 
+        }
+      });
   }
 
   public onDragStarted(index: number): void {
-   
+    this.dragging = true;
+    this.draggedIndex = index;
   }
   public onDragEnded(): void {
-  
+    this.dragging = false;
+    this.draggedIndex = null;
   }
 
   public drop(event: CdkDragDrop<Tarefa[]>): void {
+    this.onDragEnded();
+    const sourceIndex = event.previousIndex;
+    const destinationIndex = event.currentIndex;
     
+    moveItemInArray(this.tarefas, sourceIndex, destinationIndex);
+
+    const orderUpdate = {
+      sourceIndex: sourceIndex,
+      destinationIndex: destinationIndex
+    };
+
+    this.service.updateTaskOrder(orderUpdate).subscribe(() => {
+    }, () => {
+      moveItemInArray(this.tarefas, destinationIndex, sourceIndex);
+    });
   }
 
   public moveTarefa(sourceIndex: number, destinationIndex: number): void {
-    
+    moveItemInArray(this.tarefas, sourceIndex, destinationIndex);
+    const orderUpdate = { sourceIndex: sourceIndex, destinationIndex: destinationIndex };
+    this.service.updateTaskOrder(orderUpdate).subscribe(() => {
+    }, () => {
+      moveItemInArray(this.tarefas, destinationIndex, sourceIndex);
+    }); 
   }
 
 }
